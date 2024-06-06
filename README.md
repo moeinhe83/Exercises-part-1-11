cc77b7a197422eb4e8b897b4abe345f83d680989
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-06-06 20:11:27 +0330

    Upload Exercises-part-1-11 Project On Github

---
+++ README.md
@@ -0,0 +1,25 @@
+Project Name ===> Exercises Part 1
+
+Question Number ===> 11
+
+Programming Language ===> Python
+
+Program description ===> 11th Exercise
+
+Example ===> Write A Program That Checks Whether Item M Exists In The Above tuple Or not => char = ('m', 'a', 'k', 't', 'a', 'b')
+
+Description Of Files:
+
+practice_11_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python practice_11_linux.py
+
+practice_11_windows.py ===> For Windows
+
+For Run ===> python practice_11_windows.py
+
+Required packages For Python ===> os / termcolor2 / pyfiglet
+
+termcolor2 ===> To install => pip install termcolor2
+
+pyfiglet ===> To install => pip install pyfiglet
---
+++ practice_11_linux.py
@@ -0,0 +1,25 @@
+# Practice_11
+
+# Library
+from os import system as sys
+from termcolor2 import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format("Exercises Part 1"), color='cyan'))
+print(colored('=====================================================================', color='red'))
+print(colored('Question Number ===> 11', color='blue'))
+print(colored('// Be Sure To Read The README File To See The Question //', color='blue'))
+print('                                            ')
+
+# Value
+name = ('m', 'a', 'k', 't', 'a', 'b')
+# Check
+print('m' in name)
+
+# Finish
+# Create By Moein Heshmati
+
